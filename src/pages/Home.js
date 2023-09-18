6bf4cbd48dda39cadaba0502bf11030b414ee7ed
othniel-karlax1 <abs.othniel2023@gmail.com>
2023-09-18 13:31:05 +0530

    Add existing project files to Git

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,33 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../components/footer-styles/home.css'
 
 const Home = () => {
   return (
-   
-    <div>ut. Popular options include Normalize.css and Eric Meyer's CSS Reset.
-
-    Make sure you've also cleared any unwanted margins or padding from other elements on your page, especially those that come after the footer in the HTML structure.
-    
-    After making these adjustments, the white space below the footer should be removed. If the issue persists, you may need to inspect your webpage using browser developer tools to identify any additional CSS or HTML that could be causing the problem.
-    ut. Popular options include Normalize.css and Eric Meyer's CSS Reset.
-
-     Make sure you've also cleared any unwanted margins or padding from other elements on your page, especially those that come after the footer in the HTML structure.
-     
-     After making these adjustments, the white space below the footer should be removed. If the issue persists, you may need to inspect your webpage using browser developer tools to identify any additional CSS or HTML that could be causing the problem.
-     Make sure you've also cleared any unwanted margins or padding from other elements on your page, especially those that come after the footer in the HTML structure.
-    
-    After making these adjustments, the white space below the footer should be removed. If the issue persists, you may need to inspect your webpage using browser developer tools to identify any additional CSS or HTML that could be causing the problem.
-    ut. Popular options include Normalize.css and Eric Meyer's CSS Reset.
-
-     Make sure you've also cleared any unwanted margins or padding from other elements on your page, especially those that come after the footer in the HTML structure.
-     
-     Make sure you've also cleared any unwanted margins or padding from other elements on your page, especially those that come after the footer in the HTML structure.
-    
-    After making these adjustments, the white space below the footer should be removed. If the issue persists, you may need to inspect your webpage using browser developer tools to identify any additional CSS or HTML that could be causing the problem.
-    ut. Popular options include Normalize.css and Eric Meyer's CSS Reset.
-
-     Make sure you've also cleared any unwanted margins or padding from other elements on your page, especially those that come after the footer in the HTML structure.
-     
-     </div>
+    <>
+    <section className='home-wrapper-1 py-5'>
+      <div className='container-xxl'>
+        <div className='row'>
+          <div className='col-6'>
+            <div className='main-banner position-relative p-3'>
+              <img 
+              src='/images/homebanner1.png' 
+              className='new img-fluid rounded-3' 
+              alt='mainbanner'
+              />
+              <div className='main-banner-content position-absolute'>
+                <h4>LOREM IPSUM NEW PRODUCT</h4>
+                <h5>Dairy products</h5>
+                <p>from $12 - $999</p>
+                <Link>SHOP NOW</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    </>
   )
 }
 
